5c597e520d0325b9f5cbb5ed93053a52a82cf16c
GabrielS <gabrielsoares1602@gmail.com>
2023-02-18 11:56:45 -0300

    tiny colors added

--- index.js
+++ index.js
@@ -2,12 +2,88 @@ const fs = require('fs');
 const gm = require('gm').subClass({ imageMagick: '7+' });
 const xml2js = require('xml2js');
 const svg2img = require('svg2img');
+const tinycolor = require("tinycolor2");
 
-const height = 2560;
-const width = 1080;
-const backgroundColor = '#40798C';
-const svg = 'zzImagens/home-simple.svg'
+const listOfHeight16for9 = [
+  1280,
+  1366,
+  1600,
+  1920,
+  2560,
+  3840
+];
+
+const listOfWidth16for9 = [
+  720,
+  768,
+  900,
+  1080,
+  1440,
+  1800,
+];
+
+const height = listOfHeight16for9[3];
+const width = listOfWidth16for9[3];
+const svgMaxSize = width / 4;
+const backgroundColor = '#2D848A';
+const pathSvg =  'zzImagens/trident.svg'
 const backgroundOutput = 'backgroundOutput.png'
+
+const backgroundTC = tinycolor(backgroundColor);
+
+if(backgroundTC.isDark()) {
+  changeSvgStroke('#fff');
+} else {
+  changeSvgStroke('#000');
+}
+
+function changeSvgStroke(strokeColor) {
+  // Read the SVG file
+  fs.readFile(pathSvg, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Parse the XML data
+    xml2js.parseString(data, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Get the path element and set its stroke attribute to red
+      const path = result.svg.path[0];
+      // Get the <svg> element from the result object
+      const svgElement = result.svg;
+      if (path && path.$) {
+        path.$.stroke = strokeColor;
+
+        // Modify the attributes of the <svg> element
+        svgElement.$.width = svgMaxSize;
+        svgElement.$.height = svgMaxSize;
+      }
+
+
+
+      // Convert the modified XML data back to a string
+      const builder = new xml2js.Builder();
+      const modifiedData = builder.buildObject(result);
+
+      // Write the modified SVG file
+      fs.writeFile(pathSvg, modifiedData, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log('SVG stroke modificado ');
+      });
+    });
+  });
+}
+
+
 
 gm(height, width, backgroundColor)
   .write(backgroundOutput, (err) => {
@@ -15,22 +91,21 @@ gm(height, width, backgroundColor)
     if(err) console.log(err);
     console.log('background Feito');
 
-    svg2img(svg, (error, buffer) => {
+    svg2img(pathSvg, (error, buffer) => {
 
       if (error) console.log(error);
       console.log('svg2img');
-
       const tempPng = 'temp.png';
-      console.log('arquivo png temporario criado');
 
       fs.writeFileSync(tempPng, buffer);
-      console.log('requisição feita');
+      console.log('arquivo png temporario criado');
     
       // Get the dimensions of the input image and the temporary PNG file
       gm(backgroundOutput).size((err, size) => {
         if (err) console.log(err);
         console.log(size)
-        gm(tempPng).size((err, pngSize) => {
+        gm(tempPng)
+        .size((err, pngSize) => {
           if (err) console.log(err);
           console.log(pngSize)
     
@@ -39,16 +114,17 @@ gm(height, width, backgroundColor)
           console.log(x)
           const y = Math.round((size.height - pngSize.height) / 2);
           console.log(y)
-    
+          
           // Add the PNG to the image using the -geometry option
           gm(backgroundOutput)
             .geometry(`+${x}+${y}`)
             .composite(tempPng)
             // Write the output image to a file
-            .write('output.jpg', (err) => {
+            .write('output.png', (err) => {
               if (err) console.log(err);
-              console.log('Image saved successfully!');
+              console.log('Imagem salva');
               // Remove the temporary PNG file
+              fs.unlinkSync(tempPng);
               console.log('Arquivo png temporario apagado')
             });
         });
